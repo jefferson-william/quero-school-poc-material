762597118e891f42f2c879fff4bf33514307087a
Jefferson William <jeffersonwilliammachado@gmail.com>
2020-02-23 00:01:32 -0300

    Padronizando font proxima-nova

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,6 +2,9 @@ import { red } from '@material-ui/core/colors'
 import { createMuiTheme } from '@material-ui/core/styles'
 
 const theme = createMuiTheme({
+  typography: {
+    fontFamily: ['proxima-nova', 'Roboto', 'Arial', 'Helvetica', 'sans-serif'],
+  },
   palette: {
     primary: {
       main: '#304ffe',
